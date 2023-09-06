7dec7c193e5f28cf4dce122edb88bfbdefd737f0
azumew <107566380+azumew@users.noreply.github.com>
2023-09-06 22:23:52 +0900

    Update 00_initialize.cmd

--- windows/00_initialize.cmd
+++ windows/00_initialize.cmd
@@ -1,1 +1,4 @@
+# ロック画面の表示時間を変更可能にする
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7" /v Attributes /t REG_DWORD /d 2 /f
+# CapsLockキーをCtrlキーに変更する
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 0000000000000000030000001d003a003a001d0000000000 /f
