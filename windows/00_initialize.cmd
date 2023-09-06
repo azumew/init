3962b548187e1e6f743cf4a53f1c4c9c205661f3
azumew <107566380+azumew@users.noreply.github.com>
2023-09-06 19:20:24 +0900

    Create 00_initialize.cmd

---
+++ windows/00_initialize.cmd
@@ -0,0 +1,1 @@
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7" /v Attributes /t REG_DWORD /d 2 /f
